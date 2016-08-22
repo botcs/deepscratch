17311b6b7ea28473023540e24eb990bca2abaf8f
botcs <bcs@bcs.hu>
2016-08-22 23:40:28 +0200

    added netfactory script for rapid prototyping
    usage:
    python netfactory.py c 10 5 c 10 3 f 30 f 20
    will train the following network:
    
    Constructing mnist-c10x5-c10x3-f30-f20 network...
    Network ID: 140175376577488
    Network layout:
    ------------------------------
    	INPUT  (1, 28, 28)
    	   |
    	   |
    	  |1|
      convolution  (10, 24, 24)   ->   kernels: (1, 10, 5, 5)
    	   |
    	   |
    	  |2|
      activation (10, 24, 24)   ->   type: relu
    	   |
    	   |
    	  |3|
      convolution  (10, 22, 22)   ->   kernels: (10, 10, 3, 3)
    	   |
    	   |
    	  |4|
      activation (10, 22, 22)   ->   type: relu
    	   |
    	   |
    	  |5|
      shaper  (4840,)
    	   |
    	   |
    	  |6|
      fully connected  (30,)   ->   weights + bias: (30, 4840) + (30,)
    	   |
    	   |
    	  |7|
      activation (30,)   ->   type: relu
    	   |
    	   |
    	  |8|
      shaper  (30,)
    	   |
    	   |
    	  |9|
      fully connected  (20,)   ->   weights + bias: (20, 30) + (20,)
    	   |
    	   |
    	  |10|
      activation (20,)   ->   type: relu
    	   |
    	   |
    	  |11|
      shaper  (20,)
    	   |
    	   |
    	  |12|
      fully connected  (10,)   ->   weights + bias: (10, 20) + (10,)
    	   |
    	   |
    	  |13|
      	OUTPUT  (10,)   ->   CRITERION  (softmax)
    ------------------------------

---
+++ demo/mnist_test/netfactory.py
@@ -0,0 +1,100 @@
+import sys
+import numpy as np
+import network_module as nm
+
+# seq = sys.argv[1:]
+seq = ['c', '5', '10', 'f', '10']
+i = 0
+
+
+def print_csv(filename, data):
+    with open(filename, 'wb') as out:
+        for t in data:
+            out.write('{}\t{}\n'.format(*t))
+
+
+netname = 'mnist'
+i = 0
+while i < len(seq):
+    if seq[i] == 'c':
+        netname += '-c{}x{}'.format(seq[i+1],  seq[i+2])
+        i += 3
+        continue
+    elif seq[i] == 'f':
+        netname += '-f{}'.format(seq[i+1])
+        i += 2
+        continue
+    else:
+        print 'WARNING: Expected layer flag, got instead: \'' + seq[i] + '\''
+        i += 1
+
+
+
+def loadmnist():
+    import cPickle, gzip, numpy
+
+    # Load the dataset
+    f = gzip.open('./MNIST_data/mnist.pkl.gz', 'rb')
+    sets = cPickle.load(f)
+    f.close()
+    res = []
+    for set in sets:
+        set[0].shape = (-1, 1, 28, 28)
+
+        label = set[1]
+        onehot = np.zeros((label.size, label.max() + 1))
+        onehot[np.arange(label.size), label] = 1
+        res.append((set[0].reshape(-1, 1, 28, 28), onehot))
+    return res
+
+
+print 'Loading MNIST images...'
+train, valid, test = loadmnist()
+
+
+print 'Constructing', netname, 'network...'
+#########################
+# NETWORK DEFINITION
+nn = nm.network(in_shape=train[0][0].shape, criterion='softmax')
+i = 0
+while i < len(seq):
+    if seq[i] == 'c':
+        nn.add_conv(int(seq[i+1]), (int(seq[i+2]), int(seq[i+2])))
+        i += 3
+        continue
+    elif seq[i] == 'f':
+        nn.add_shaper(np.prod(nn[-1].shape))
+        nn.add_full(int(seq[i+1]))
+        i += 2
+        continue
+    else:
+        print 'WARNING: Expected layer flag, got instead: \'' + seq[i] +'\''
+        i += 1
+
+    nn.add_activation('relu')
+
+
+nn.add_shaper(np.prod(nn[-1].shape))
+nn.add_full(10)
+#########################
+print nn
+
+result = []
+
+
+def print_test():
+    train_score = nn.test_eval(train)
+    test_score = nn.test_eval(test)
+    print nn.last_epoch, train, test
+    result.append(train_score, test_score)
+
+
+print 'Training network', netname
+nn.SGD(train_policy=nn.fix_epoch, training_set=train,
+       batch=16, rate=0.05, epoch_call_back=print_test, epoch=4)
+
+print 'Saving results to {}.res'.format(netname)
+print_csv('./results/{}.res'.format(netname), result)
+
+print 'Saving network snapshot to {}.net'.format(netname)
+nn.save_state('./nets/' + netname + '.net')
